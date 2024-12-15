ef4dbbc062a272662006daee727c80d7c80e9d68
Yashi Gupta <yashi.g23csai@nst.rishihood.edu.in>
2024-12-15 23:35:59 +0530

    assignment try 1

--- index.js
+++ index.js
@@ -1,13 +1,74 @@
 const express = require('express');
 const dotenv = require('dotenv'); 
+const bcrypt = require('bcrypt');
+const { prisma } = require("./db/config.js");
+const jwt = require('jsonwebtoken');
 dotenv.config(); 
 
 const app = express();
-
-
 app.use(express.json()); 
 
 const PORT = process.env.PORT || 3000;  
+
+app.post("/api/auth/signup", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    if(!email || !password) {
+      return res.status(400).json({ "error": "Email is required" });
+    }
+    const isThere = await prisma.users.findUnique({
+      where: {
+        email: email
+      }
+    });
+    if(isThere) {
+      return res.status(400).json({ "error": "Email already in use" });
+    }
+    const hashedPass = await bcrypt.hash(password, 10);
+    const user = await prisma.users.create({
+      data: {
+        name: name,
+        email: email,
+        password: hashedPass
+      }
+    })
+    return res.status(201).json({ "message": "User created successfully", "userId": user.id });
+  }
+  catch(err) {
+    console.log(err);
+    return res.status(500).json({ "Error": "Internal server error" })
+  }
+});
+
+app.post("/api/auth/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const jwtToken = process.env.JWT_TOKEN;
+    if(!email || !password) {
+      return res.status(400).json({ "error": "Email and password are required" });
+    }
+    const isThere = await prisma.users.findUnique({
+      where: {
+        email: email
+      }
+    });
+    if(!isThere) {
+      return res.status(404).json({ "error": "User not found" });
+    }
+    const isPassword = await bcrypt.compare(password, isThere.password);
+    if(!isPassword) {
+      return res.status(401).json({ "error": "Invalid credentials" });
+    }
+
+    const accessToken = jwt.sign({ name: isThere.name, email, password }, jwtToken);
+    return res.status(200).json({ "userdata": { "id": isThere.id, "name": isThere.name, email }, accessToken });
+  }
+  catch(err) {
+    console.log(err);
+    return res.status(500).json({ "Error": "Internal server error" })
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Backend server is running at http://localhost:${PORT}`);
 });
